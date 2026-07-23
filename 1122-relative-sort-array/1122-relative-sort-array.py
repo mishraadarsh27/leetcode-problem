bc1d32e005ae55227059ce9734fa485db3ff9c8d
Adarsh kumar <adarsh22mishra@gmail.com>
2026-07-23 19:27:20 +0530

    Time: 0 ms (100%), Space: 19.4 MB (16%) - LeetHub

---
+++ 1122-relative-sort-array/1122-relative-sort-array.py
@@ -0,0 +1,13 @@
+class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        ans, cnt = [], collections.Counter(arr1)
+        for i in arr2:
+            for _ in range(cnt.pop(i)):
+                ans.append(i)
+        for i in range(min(arr1),max(arr1) + 1):
+            for _ in range(cnt.pop(i, 0)):
+                ans.append(i)
+        return ans
+            
+
+        
