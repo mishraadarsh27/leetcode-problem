26c12e5fa08d51038a8429331f69b02885af68ac
Adarsh kumar <adarsh22mishra@gmail.com>
2026-07-24 12:41:08 +0530

    Time: 1 ms (60.87%), Space: 19.3 MB (74.95%) - LeetHub

---
+++ 1619-mean-of-array-after-removing-some-elements/1619-mean-of-array-after-removing-some-elements.py
@@ -0,0 +1,8 @@
+class Solution:
+    def trimMean(self, arr: List[int]) -> float:
+        arr.sort()
+        n = len(arr)
+        per = int(n*5/100)
+        l2 = arr[per:len(arr) - per]
+        x = sum(l2) / len(l2)
+        return x
