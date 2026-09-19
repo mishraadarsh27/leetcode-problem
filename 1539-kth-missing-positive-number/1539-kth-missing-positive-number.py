bbfee20ec8e7aca84eed35afebd9a0605d8173d8
Adarsh kumar <adarsh22mishra@gmail.com>
2026-09-19 23:14:24 +0530

    Time: 0 ms (100%), Space: 19.5 MB (10.33%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.py
@@ -1,4 +1,9 @@
 class Solution:
-    def findKthPositive(self, arr: list[int], k: int) -> int:
-        return bisect_right(range(len(arr)), k, key= lambda x: arr[x] - x) + k
+    def findKthPositive(self, arr: List[int], k: int) -> int:
+        lo, hi = 0, len(arr)
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if arr[mid] - mid > k: hi = mid
+            else: lo = mid + 1
+        return lo + k
         
